2ff0b9e63800c75c4d89ea74119384fcc2f27e85
antoforestello <anto.forestello@gmail.com>
2022-09-21 14:48:30 -0300

    agregar modificacion B

--- src/clases/HelloWorld.java
+++ src/clases/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public String nombre;
 	public String apellido;
 	public boolean modificacionA=true;
+	public boolean modificacionB=true;
 	public HelloWorld() {
 		nombre="Juan";
 		apellido="Perez";
